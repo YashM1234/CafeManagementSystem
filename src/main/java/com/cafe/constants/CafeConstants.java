381d0e2afd28be1f29d4d62c1352afadfa9d764e
YashM1234 <yashmungolerpg@gmail.com>
2024-06-01 13:32:30 +0530

    Implement signup functionality and swagger for api documentation...

---
+++ src/main/java/com/cafe/constants/CafeConstants.java
@@ -0,0 +1,13 @@
+package com.cafe.constants;
+
+public class CafeConstants {
+
+    private CafeConstants() {
+
+    }
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+    public static final String INVALID_DATA = "Invalid data";
+    public static final String EMAIL_ALREADY_EXIST = "Email already exists";
+    public static final String EMAIL = "email";
+}
+
---
+++ src/main/java/com/cafe/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.cafe.controller;
+
+import com.cafe.constants.CafeConstants;
+import com.cafe.service.UserService;
+import com.cafe.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap){
+
+        try{
+
+            return userService.signUp(requestMap);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
