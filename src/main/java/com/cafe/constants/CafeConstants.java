ef7a4c9b01f6b9e629bcfb83286f0d539760b6f1
YashM1234 <yashmungolerpg@gmail.com>
2024-06-06 15:17:38 +0530

    Implement All Billing Management APIs...

--- src/main/java/com/cafe/constants/CafeConstants.java
+++ src/main/java/com/cafe/constants/CafeConstants.java
@@ -6,9 +6,15 @@ public class CafeConstants {
 
     }
     public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+
     public static final String INVALID_DATA = "Invalid data";
+
     public static final String EMAIL_ALREADY_EXIST = "Email already exists";
+
     public static final String EMAIL = "email";
+
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
+
+    public static final String STORE_LOCATION = "src/main/resources/bill_pdf";
 }
 
---
+++ src/main/java/com/cafe/repository/BillRepository.java
@@ -0,0 +1,14 @@
+package com.cafe.repository;
+
+import com.cafe.entity.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BillRepository extends JpaRepository<Bill, Integer> {
+
+    List<Bill> getAllBills();
+
+    List<Bill> getBillByUserName(@Param("name") String name);
+}
